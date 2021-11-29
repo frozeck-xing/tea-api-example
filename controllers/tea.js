89f18fa02ace453aee3edbaa5c2795f8213cb557
Frank Perez <frank.perez.developer@gmail.com>
2021-11-29 17:37:16 -0500

    Nov 26 - Created controllers for Post, Get, Delete

--- controllers/tea.js
+++ controllers/tea.js
@@ -1,31 +1,100 @@
+const { json } = require('express');
+const TeaModel = require('../models/tea');
+
 // GET '/tea'
 const getAllTeas = (request, response, next) => {
-    response.json({message: "GET all teas"});
+    TeaModel.find({}, (error, data) => {
+        if(error){
+            return response.json({Error: error});
+        }
+
+        return response.json(data);
+    });
 };
 
 // POST '/tea'
 const newTea = (request, response, next) => {
-    response.json({message: "POST new tea"});
+
+    TeaModel.findOne({name: request.body.name}, (error, data) => {
+        if(!data){
+            const newTea= new TeaModel({
+                name: request.body.name,
+                image: request.body.image,
+                description: request.body.description,
+                keywords: request.body.keywords,
+                origin: request.body.origin,
+                brew_time: request.body.brew_time,
+                temperature: request.body.temperature,
+            });
+
+            newTea.save((error, data) => {
+                if(error) return response.json({message: "Couldn't add the new tea.", reason: error});
+                return response.json(data);
+            });
+
+        } else {
+            if(error) return response.json(`There was an error saving the new tea, please try again. ${error}.`);
+            return response.json({message: "Tea already exist"});
+        }
+    });
 };
 
 // DELETE '/tea'
 const deleteAllTeas = (request, response, next) => {
-    response.json({message: "DELETE all teas"});
+    TeaModel.deleteMany({}, error => {
+        if(error) return response.json({message: "Complete delete failed", reason: error});
+
+        return response.json({message: "Complete delete successful."});
+    });
 };
 
 // GET '/tea:name'
 const getOneTea = (request, response, next) => {
-    response.json({message: "GET 1 tea"});
+   const name = request.params?.name.toLowerCase();
+
+   TeaModel.findOne({name: name}, (error, data) => {
+    if(error || !data) return response.json({message: "Tea doesn't exist.", reason: error});
+
+    return response.json(data);
+   });
 };
 
 // POST '/tea:name'
 const newComment = (request, response, next) => {
-    response.json({message: "POST a new comment on the tea"});
+    const name = request.params?.name.toLowerCase();
+    const comment = request.body?.comment;
+
+    const newComment = {
+        text: comment,
+        date: new Date()
+    };
+
+    TeaModel.findOne({name: name}, (error, data) => {
+        if(error || !data || !comment){
+            return response.json({message: "Tea doesn't exist.", reason: error});
+        } else {
+            data.comments.push(newComment);
+
+            data.save(error => {
+                if(error) return response.json({message: "Comment failed to add.", reason: error});
+
+                return response.json(data);
+            })
+        }
+    });
 };
 
 // DELETE '/tea:name'
 const deleteOneTea = (request, response, next) => {
-    response.json({message: "DELETE this one tea"});
+    const name = request.params?.name.toLowerCase();
+
+    TeaModel.deleteOne({name: name}, (error, data) => {
+        if(data.deletedCount === 0) return response.json({message: "Tea doesn't exist", reason: error});
+
+        if(error) return response.json({message: "Something went wrong, please try again.", reason: error});
+
+        return response.json({message: "Tea deleted."});
+    });
 };
 
 module.exports = {newTea, getAllTeas, deleteAllTeas, getOneTea, newComment, deleteOneTea};
